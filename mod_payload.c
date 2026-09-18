6699784215775cf8f76552582e752806e7cb5bd7
alialauren1 <aliawolken14@gmail.com>
2026-09-18 16:02:28 -0700

    2026 09 18 AW payload ctrl_meas/sleep sending to payload over UART

--- mod_payload.c
+++ mod_payload.c
@@ -19,9 +19,33 @@
 #define PAYLOAD_OUTPUT_PORT  gpioPortA
 #define PAYLOAD_OUTPUT_PIN   13
 
+#define PAYLOAD_WAKE_MS        150  // after "@@@@@@" (from adcp.py)
+#define PAYLOAD_BREAK_GAP_MS   400  // between the two "K1W%!Q" (from adcp.py)
+#define PAYLOAD_REPLY_WAIT_MS  500  // TEMP: stands in for reading the reply
+
+static uint8_t payload_wake_str[]      = "@@@@@@";
+static uint8_t payload_break_str[]     = "K1W%!Q";
+static uint8_t payload_mc_str[]        = "MC\r\n";
+static uint8_t payload_start_str[]     = "START\r\n";
+static uint8_t payload_powerdown_str[] = "POWERDOWN\r\n";
+
 static payload_state_t payload_commanded = PAYLOAD_STATE_UNKNOWN;
 
 payload_state_t payload_get_commanded(void) { return payload_commanded; }
+
+// Nortek break: wake characters, then the break string twice
+static void payload_break(void)
+{
+  RTOS_ERR err;
+  UARTDRV_TransmitB(sl_uartdrv_usart_payload_handle, payload_wake_str, sizeof(payload_wake_str) - 1);
+  OSTimeDly(PAYLOAD_WAKE_MS, OS_OPT_TIME_DLY, &err);
+  UARTDRV_TransmitB(sl_uartdrv_usart_payload_handle, payload_break_str, sizeof(payload_break_str) - 1);
+  OSTimeDly(PAYLOAD_BREAK_GAP_MS, OS_OPT_TIME_DLY, &err);
+  UARTDRV_TransmitB(sl_uartdrv_usart_payload_handle, payload_break_str, sizeof(payload_break_str) - 1);
+  OSTimeDly(PAYLOAD_REPLY_WAIT_MS, OS_OPT_TIME_DLY, &err); // TODO: read the reply banner instead
+}
+
+// -----------------------------------------------------------------------------------
 
 bool payload_init(void)
 {
@@ -29,14 +53,10 @@ bool payload_init(void)
   GPIO_PinModeSet(PAYLOAD_OUTPUT_PORT, PAYLOAD_OUTPUT_PIN, gpioModePushPull, 1); // HIGH = instrument ON
   payload_commanded = PAYLOAD_STATE_MEASURING;
 
-  // TEMP bench test: send the line 10 times, 0.5 s apart (delays boot ~5 s)
-  RTOS_ERR err;
-  static uint8_t test_msg[] = "UUUU payload UART0 test\r\n";
-  for (int i = 0; i < 10; i++) {
-      Ecode_t ec = UARTDRV_TransmitB(sl_uartdrv_usart_payload_handle, test_msg, sizeof(test_msg) - 1);
-      printf("payload UART0 test %d sent, ecode=%lu\r\n", i, (unsigned long)ec);
-      OSTimeDly(500, OS_OPT_TIME_DLY, &err);
-  }
+  // TEMP bench test: send the line once
+  static uint8_t test_msg[] = "payload UART0 test\r\n";
+  Ecode_t ec = UARTDRV_TransmitB(sl_uartdrv_usart_payload_handle, test_msg, sizeof(test_msg) - 1);
+  printf("payload UART0 test sent, ecode=%lu\r\n", (unsigned long)ec);
 
   // TODO software switch: DONT assume state of payload.
   // Serial payload runs through MCU reset so on startup payload may be in measurement mode.
@@ -49,7 +69,13 @@ bool payload_init(void)
 // sets the payload ON or in measurement mode
 bool payload_ctrl_meas(void)
 {
+  RTOS_ERR err;
   GPIO_PinOutSet(PAYLOAD_OUTPUT_PORT, PAYLOAD_OUTPUT_PIN); // HIGH = instrument ON
+
+  payload_break();
+  UARTDRV_TransmitB(sl_uartdrv_usart_payload_handle, payload_mc_str, sizeof(payload_mc_str) - 1);    // TODO: only if the break reply says Confirmation mode
+  OSTimeDly(PAYLOAD_REPLY_WAIT_MS, OS_OPT_TIME_DLY, &err);                                           // TODO: wait for "OK" instead
+  UARTDRV_TransmitB(sl_uartdrv_usart_payload_handle, payload_start_str, sizeof(payload_start_str) - 1);
   payload_commanded = PAYLOAD_STATE_MEASURING;
   return true;
 }
@@ -57,7 +83,12 @@ bool payload_ctrl_meas(void)
 // sets payload OFF or out of measurement mode
 bool payload_ctrl_sleep(void)
 {
+  RTOS_ERR err;
   GPIO_PinOutClear(PAYLOAD_OUTPUT_PORT, PAYLOAD_OUTPUT_PIN); // LOW = instrument OFF
+  payload_break();
+  UARTDRV_TransmitB(sl_uartdrv_usart_payload_handle, payload_mc_str, sizeof(payload_mc_str) - 1);    // TODO: only if the break reply says Confirmation mode
+  OSTimeDly(PAYLOAD_REPLY_WAIT_MS, OS_OPT_TIME_DLY, &err);                                           // TODO: wait for "OK" instead
+  UARTDRV_TransmitB(sl_uartdrv_usart_payload_handle, payload_powerdown_str, sizeof(payload_powerdown_str) - 1);
   payload_commanded = PAYLOAD_STATE_SLEEP;
   return true;
 }
